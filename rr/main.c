3350a20ec229035612126266c011cacdeb982bfb
pabloegpf1 <pabloescrivagallardo@gmail.com>
2020-03-27 16:18:10 +0100

    main_rr

--- rr/main.c
+++ rr/main.c
@@ -15,7 +15,27 @@ extern void function_thread_eject(int sec);
 
 int main(int argc, char *argv[])
 {
+  int j,k,a,b;
 
+  mythread_setpriority(LOW_PRIORITY);
+  if((j = mythread_create(function_thread_eject,HIGH_PRIORITY, 1)) == -1){
+      printf("thread failed to initialize\n");
+      exit(-1);
+  }
+
+  if((k = mythread_create(function_thread,HIGH_PRIORITY, 1)) == -1){
+    printf("thread failed to initialize\n");
+    exit(-1);
+  }
+
+  for (a=0; a<10; ++a) {
+    for (b=0; b<30000000; ++b);
+  }
+  
+  mythread_exit();	
+  
+  printf("This program should never come here\n");
+  
   return 0;
 } /****** End main() ******/
 
